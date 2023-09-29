c52d17d43b14a93e5593fd921bba0965648bfeba
lucasscabral <lucas.9cabral@gmail.com>
2023-09-29 11:48:54 -0300

    feat:Implementa mais exceções de erros do prisma

--- projeto-sice-back-end/src/exceptions-filters/prisma.exception-filter.ts
+++ projeto-sice-back-end/src/exceptions-filters/prisma.exception-filter.ts
@@ -1,21 +1,32 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from "@nestjs/common";
-import {PrismaClientKnownRequestError} from '@prisma/client/runtime/library'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 
 
 @Catch(PrismaClientKnownRequestError)
-export class PrismaExceptionFilter implements ExceptionFilter{
+export class PrismaExceptionFilter implements ExceptionFilter {
     catch(exception: PrismaClientKnownRequestError, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse();
 
-        if(exception.code === 'P2025'){
+        if (exception.code === 'P2025') {
             return response.status(404).json({
                 statusCode: 404,
                 message: exception.message
             })
+        } else if (exception.code === 'P2002') {
+            return response.status(401).json({
+                statusCode: 401,
+                message: "Esse CPF já está em uso"
+            })
+        } else if (exception.code === 'P2003') {
+            return response.status(404).json({
+                statusCode: 404,
+                message: exception.message,
+                url: exception.meta
+            })
         }
         return response.status(500).json({
-            statusCode:500,
+            statusCode: 500,
             message: 'Internal server error'
         })
     }
