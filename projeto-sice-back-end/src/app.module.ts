e80d04814cfbad978aee0cf0bac865e7c03172c7
lucasscabral <lucas.9cabral@gmail.com>
2023-09-29 11:41:15 -0300

    feat:Adiciona o modulo de funcionarios no AppModule

--- projeto-sice-back-end/src/app.module.ts
+++ projeto-sice-back-end/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [ProductsModule, PrismaModule],
+  imports: [ProductsModule, PrismaModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
