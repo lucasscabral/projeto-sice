baeaa30261d0affb073ea1bc0357a5a7b5c59271
lucasscabral <lucas.9cabral@gmail.com>
2023-10-10 12:38:58 -0300

    feat:Implementa rota do estoque e config inicial das notificações do projeto

--- projeto-sice-front-end/src/App.jsx
+++ projeto-sice-front-end/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import IndexHeader from './components/header'
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom'
+import IndexStock from './components/stock/indexStock'
+import Notiflix from 'notiflix'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,10 +14,60 @@ function App() {
         <IndexHeader />
         <Routes>
           <Route path='/home' element={<div>Home</div>} />
+          <Route path='/estoque' element={<IndexStock />} />
         </Routes>
       </BrowserRouter>
     </>
   )
 }
+
+Notiflix.Notify.init({
+  width: '280px',
+  position: 'right-bottom',
+  distance: '10px',
+  opacity: 1,
+  borderRadius: '5px',
+  rtl: false,
+  timeout: 3000,
+  messageMaxLength: 110,
+  backOverlay: false,
+  backOverlayColor: 'rgba(0,0,0,0.5)',
+  plainText: true,
+  showOnlyTheLastOne: false,
+  clickToClose: false,
+  pauseOnHover: true,
+  ID: 'NotiflixNotify',
+  className: 'notiflix-notify',
+  zindex: 4001,
+  fontFamily: 'Quicksand',
+  fontSize: '13px',
+  cssAnimation: true,
+  cssAnimationDuration: 400,
+  cssAnimationStyle: 'fade',
+  closeButton: false,
+  useIcon: true,
+  useFontAwesome: false,
+  fontAwesomeIconStyle: 'basic',
+  fontAwesomeIconSize: '34px',
+  success: {
+    background: '#32c682',
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-success',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-check-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(50,198,130,0.2)',
+  },
+  failure: {
+    background: '#ff5549',
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-failure',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-times-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(255,85,73,0.2)',
+  }
+});
+
 
 export default App
