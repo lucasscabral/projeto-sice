55402699d8a64da47c40fcbfbc4bb8235a90402e
lucasscabral <lucas.9cabral@gmail.com>
2023-10-11 17:26:08 -0300

    feat:Implementa as ações para os butões de visualizar,editar e excluir de um produto

--- projeto-sice-front-end/src/components/stock/buttonActions.jsx
+++ projeto-sice-front-end/src/components/stock/buttonActions.jsx
@@ -1,34 +1,44 @@
 import { Stack, Tooltip } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { Create } from "@mui/icons-material";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import ModalDelete from "./modal/modalDelete";
-
+import { AiOutlineInfoCircle, AiOutlineForm } from "react-icons/ai";
+import FormEditionProduct from "./forms/formEditionProduct";
+import ModalDetails from "./modal/modalDetails";
 
 export default function ButtonActions({
-    params,
+    _,
     refetch,
     modification,
     setModification,
 }) {
-    const navigate = useNavigate();
+
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+
+    const [openFormEdit, setOpenFormEdit] = useState(false);
+    const handleOpenFormEdit = () => setOpenFormEdit(true);
+    const handleCloseFormEdit = () => setOpenFormEdit(false);
+
+    const [openModalDetails, setOpenModalDetails] = useState(false);
+    const handleOpenModalDetails = () => setOpenModalDetails(true);
+    const handleCloseModalDetails = () => setOpenModalDetails(false);
+
     const getRowSelectedOfLocalStorage = localStorage.getItem("rowSelected");
     const parseRowSelected = JSON.parse(getRowSelectedOfLocalStorage);
 
-    const onClick = (_) => {
-        const currentRow = params.row;
-        navigate(`/dashboard/pescadores/editar/${currentRow?.idprodutos}`);
-    };
     return (
         <Stack direction="row" alignItems="center" spacing={0}>
-            <Tooltip title="Editar" onClick={onClick}>
+            <Tooltip title="Ver Descrição" onClick={handleOpenModalDetails}>
+                <IconButton aria-label="visualizar" size="medium" color="primary">
+                    <AiOutlineInfoCircle color="#D9430D" />
+                </IconButton>
+            </Tooltip>
+            <Tooltip title="Editar" onClick={handleOpenFormEdit}>
                 <IconButton aria-label="editar" size="medium" color="primary">
-                    <Create fontSize="inherit" />
+                    <AiOutlineForm />
                 </IconButton>
             </Tooltip>
             <Tooltip title="Deletar">
@@ -43,14 +53,28 @@ export default function ButtonActions({
             </Tooltip>
             {open ? (
                 <ModalDelete
-                    handleClose={handleClose}
                     open={open}
+                    handleClose={handleClose}
                     rowId={parseRowSelected?.row.idprodutos}
                     refetch={refetch}
                     modification={modification}
                     setModification={setModification}
                 />
             ) : null}
+            {openFormEdit ? (
+                <FormEditionProduct
+                    openFormEdit={openFormEdit}
+                    handleCloseFormEdit={handleCloseFormEdit}
+                    rowId={parseRowSelected?.row.idprodutos}
+                    refetch={refetch} />
+            ) : null}
+            {openModalDetails ? (
+                <ModalDetails
+                    openModalDetails={openModalDetails}
+                    handleCloseModalDetails={handleCloseModalDetails}
+                    rowId={parseRowSelected?.row.idprodutos} />
+            ) : null
+            }
         </Stack>
     );
 }
