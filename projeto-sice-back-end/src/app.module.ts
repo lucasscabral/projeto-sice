f0c3ef4de8dcbc6a61f2e0554f39114d0c3e1fab
lucasscabral <lucas.9cabral@gmail.com>
2023-10-19 03:11:00 -0300

    feat:Importa o modulo de vendas no appModule

--- projeto-sice-back-end/src/app.module.ts
+++ projeto-sice-back-end/src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { EmployeesModule } from './employees/employees.module';
 import { OfficeModule } from './office/office.module';
 import { CategoryModule } from './category/category.module';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [ProductsModule, PrismaModule, EmployeesModule, OfficeModule, CategoryModule],
+  imports: [ProductsModule, PrismaModule, EmployeesModule, OfficeModule, CategoryModule, SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
