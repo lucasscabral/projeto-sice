f6f86b886f87a2adebc5ea981c759c994cf503ab
lucasscabral <lucas.9cabral@gmail.com>
2023-10-19 15:14:32 -0300

    feat:implementa o componente de listagem de produtos e os butões de ação para cada item

---
+++ projeto-sice-front-end/src/components/box/table/buttonsActions.jsx
@@ -0,0 +1,63 @@
+import { Stack, Tooltip } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useState } from "react";
+import { AiOutlineForm } from "react-icons/ai";
+import ModalDeleteItem from "../modal/modalDeleteItem";
+import FormEditionProduct from "../form/formEditionProduct";
+
+export default function ButtonActions({
+    params,
+    productsSelected,
+    setProductsSelected
+}) {
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const [openFormEdit, setOpenFormEdit] = useState(false);
+    const handleOpenFormEdit = () => setOpenFormEdit(true);
+    const handleCloseFormEdit = () => setOpenFormEdit(false);
+
+    return (
+        <Stack direction="row" alignItems="center" spacing={0}>
+            <Tooltip title="Editar" onClick={handleOpenFormEdit}>
+                <IconButton aria-label="editar" size="medium" color="primary">
+                    <AiOutlineForm />
+                </IconButton>
+            </Tooltip>
+            <Tooltip title="Deletar">
+                <IconButton
+                    aria-label="deletar"
+                    size="medium"
+                    color="error"
+                    onClick={handleOpen}
+                >
+                    <DeleteIcon fontSize="inherit" />
+                </IconButton>
+            </Tooltip>
+            {open ? (
+                <ModalDeleteItem
+                    open={open}
+                    handleClose={handleClose}
+                    row={params?.row}
+                    productsSelected={productsSelected}
+                    setProductsSelected={setProductsSelected}
+                />
+            ) : null}
+            {openFormEdit ? (
+                <FormEditionProduct
+                    openFormEdit={openFormEdit}
+                    handleCloseFormEdit={handleCloseFormEdit}
+                    row={params?.row}
+                    productsSelected={productsSelected}
+                    setProductsSelected={setProductsSelected}
+                />
+            ) : null}
+        </Stack>
+    );
+}
+
+
+
---
+++ projeto-sice-front-end/src/components/box/table/tableProducts.jsx
@@ -0,0 +1,49 @@
+import { Box, Typography } from "@mui/material";
+import { DataGrid, GridLogicOperator } from "@mui/x-data-grid";
+import { AiOutlineInbox } from "react-icons/ai";
+
+function CustomNoRowsOverlay() {
+    return (
+        <Box
+            sx={{
+                width: 1,
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+            }}
+        >
+            <AiOutlineInbox size={100} style={{ marginBottom: "15px" }} />
+            <Typography variant="h6">Caixa Vazio</Typography>
+        </Box>
+    );
+}
+
+
+export default function TableProducts({ rows, columns }) {
+    return (<DataGrid
+        sx={{ p: 3, marginTop: 5 }}
+        autoHeight
+        rows={rows}
+        columns={columns}
+        getRowId={(rows) => rows.idprodutos}
+        disableRowSelectionOnClick
+        disableColumnMenu
+        initialState={{
+            pagination: {
+                paginationModel: { page: 0, pageSize: 5 },
+            }, filter: {
+                filterModel: {
+                    items: [],
+                    quickFilterLogicOperator: GridLogicOperator.Or,
+                },
+            },
+        }}
+        slots={{
+            noRowsOverlay: CustomNoRowsOverlay,
+            noResultsOverlay: CustomNoRowsOverlay,
+        }}
+        pageSizeOptions={[5, 10]}
+    />)
+}
