5271868f0ac31130a11d89c31749a781d3ac5407
lucasscabral <lucas.9cabral@gmail.com>
2023-10-22 01:36:03 -0300

    feat:Implementa as configs do jwt no modulo de funcionario

--- projeto-sice-back-end/src/employees/employees.module.ts
+++ projeto-sice-back-end/src/employees/employees.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports:[JwtModule.register({
+    global: true,
+    secret: process.env.JWT_KEY_SECRET,
+    signOptions: { expiresIn: '68878484826s' },
+  }),],
   controllers: [EmployeesController],
   providers: [EmployeesService],
 })
