e5461967bb6213c8f56b03f69605751dbdd33428
lucasscabral <lucas.9cabral@gmail.com>
2023-11-22 01:13:32 -0300

    feat:Cria componente de fornecedores

---
+++ projeto-sice-front-end/src/components/suppliers/indexSuppliers.jsx
@@ -0,0 +1,102 @@
+import {
+  Box,
+  Button,
+  Divider,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
+import FormCreateSupplier from "./forms/formCreateSupplier";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const theme = createTheme({
+  palette: {
+    info: {
+      main: "#00A09D",
+    },
+    success: {
+      main: "#00A09D",
+    },
+  },
+});
+
+export default function IndexSuppliers() {
+  const [openModalCreateSupplier, setOpenModalCreateSupplier] = useState(false);
+  const handleOpenModalCreateSupplier = () => setOpenModalCreateSupplier(true);
+  const handleCloseModalCreateSupplier = () =>
+    setOpenModalCreateSupplier(false);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box
+        marginTop={15}
+        paddingLeft={10}
+        paddingRight={10}
+        sx={{ width: "100%" }}
+      >
+        <Typography variant="h4" fontWeight={"bold"}>
+          Fornecedores
+        </Typography>
+        <Divider variant="fullWidth" color="#232723" sx={{ marginTop: 2 }} />
+
+        <Box
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "flex-start",
+            marginTop: 8,
+            gap: 4,
+            flexDirection: "column",
+          }}
+        >
+          <Button
+            variant="contained"
+            size="large"
+            color="info"
+            sx={{ fontWeight: "bold", width: 250, height: 50 }}
+          >
+            <Link
+              to={"lista"}
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              Fornecedores
+            </Link>
+          </Button>
+          <Button
+            variant="contained"
+            size="large"
+            color="info"
+            sx={{ fontWeight: "bold", width: 250, height: 50 }}
+            onClick={handleOpenModalCreateSupplier}
+          >
+            Cadastrar Novo Fornecedores
+          </Button>
+          <Button
+            variant="contained"
+            size="large"
+            color="info"
+            sx={{ fontWeight: "bold", width: 250, height: 50 }}
+          >
+            Registrar Compra
+          </Button>
+          <Button
+            variant="contained"
+            size="large"
+            color="info"
+            sx={{ fontWeight: "bold", width: 250, height: 50 }}
+          >
+            Listagem de Compras
+          </Button>
+        </Box>
+      </Box>
+      {openModalCreateSupplier ? (
+        <FormCreateSupplier
+          openModalCreateSupplier={openModalCreateSupplier}
+          handleCloseModalCreateSupplier={handleCloseModalCreateSupplier}
+        />
+      ) : null}
+    </ThemeProvider>
+  );
+}
