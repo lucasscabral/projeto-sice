4391c26a307b11ee665d1ca236550e77fa63e79d
lucasscabral <lucas.9cabral@gmail.com>
2023-11-22 01:14:36 -0300

    fix:Formata o arquivo

--- projeto-sice-front-end/src/utils/tableUtils.jsx
+++ projeto-sice-front-end/src/utils/tableUtils.jsx
@@ -1,88 +1,87 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import {
-    DataGrid,
-    GridToolbarQuickFilter,
-    GridLogicOperator,
+  DataGrid,
+  GridToolbarQuickFilter,
+  GridLogicOperator,
 } from "@mui/x-data-grid";
 import { AiOutlineInbox } from "react-icons/ai";
 import { Typography } from "@mui/material";
 
 function QuickSearchToolbar() {
-    return (
-        <Box
-            sx={{
-                p: 1,
-                pb: 0,
-                mb: 3,
-            }}
-        >
-            <GridToolbarQuickFilter
-                sx={{
-                    border: "1px lightgray solid",
-                    borderRadius: 3,
-                    p: 1,
-                }}
-                placeholder="Buscar por Id ou nome"
-                quickFilterParser={(searchInput) =>
-                    searchInput
-                        .split(",")
-                        .map((value) => value.trim())
-                        .filter((value) => value !== "")
-                }
-            />
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        p: 1,
+        pb: 0,
+        mb: 3,
+      }}
+    >
+      <GridToolbarQuickFilter
+        sx={{
+          border: "1px lightgray solid",
+          borderRadius: 3,
+          p: 1,
+        }}
+        placeholder="Buscar por Id ou nome"
+        quickFilterParser={(searchInput) =>
+          searchInput
+            .split(",")
+            .map((value) => value.trim())
+            .filter((value) => value !== "")
+        }
+      />
+    </Box>
+  );
 }
 
 function CustomNoRowsOverlay() {
-    return (
-        <Box
-            sx={{
-                width: 1,
-                height: "100%",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-            }}
-        >
-            <AiOutlineInbox size={100} style={{ marginBottom: "15px" }} />
-            <Typography variant="h6">Sem Registros</Typography>
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        width: 1,
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <AiOutlineInbox size={100} style={{ marginBottom: "15px" }} />
+      <Typography variant="h6">Sem Registros</Typography>
+    </Box>
+  );
 }
 
-
 export default function TableUtils({ dataContent, columns, setRowSelected }) {
-    return (
-        <Box sx={{ width: 1, marginTop: 5 }}>
-            <DataGrid
-                autoHeight
-                sx={{ p: 3 }}
-                getRowId={(rows) => rows.idprodutos}
-                columns={columns}
-                rows={dataContent}
-                disableRowSelectionOnClick
-                disableColumnMenu
-                key={dataContent}
-                pageSizeOptions={[10, 25, 50]}
-                initialState={{
-                    pagination: { paginationModel: { pageSize: 10 } },
-                    filter: {
-                        filterModel: {
-                            items: [],
-                            quickFilterLogicOperator: GridLogicOperator.Or,
-                        },
-                    },
-                }}
-                slots={{
-                    toolbar: QuickSearchToolbar,
-                    noRowsOverlay: CustomNoRowsOverlay,
-                    noResultsOverlay: CustomNoRowsOverlay,
-                }}
-                onRowClick={(params) => setRowSelected(params)}
-            />
-        </Box>
-    );
+  return (
+    <Box sx={{ width: 1, marginTop: 5 }}>
+      <DataGrid
+        autoHeight
+        sx={{ p: 3 }}
+        getRowId={(rows) => rows.idprodutos}
+        columns={columns}
+        rows={dataContent}
+        disableRowSelectionOnClick
+        disableColumnMenu
+        key={dataContent}
+        pageSizeOptions={[10, 25, 50]}
+        initialState={{
+          pagination: { paginationModel: { pageSize: 10 } },
+          filter: {
+            filterModel: {
+              items: [],
+              quickFilterLogicOperator: GridLogicOperator.Or,
+            },
+          },
+        }}
+        slots={{
+          toolbar: QuickSearchToolbar,
+          noRowsOverlay: CustomNoRowsOverlay,
+          noResultsOverlay: CustomNoRowsOverlay,
+        }}
+        onRowClick={(params) => setRowSelected(params)}
+      />
+    </Box>
+  );
 }
